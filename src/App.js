fadc556079505c6c040c2e7763c393a6a7735902
Ngozi Okafor <ngoziokafor2014@gmail.com>
2022-01-21 17:42:16 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [hex, setHex] = useState('#ffffff');
+  const randomizedHex = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setHex(randomColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="Container"
+      style={{
+        backgroundColor: `${hex}`,
+        minHeight: '35.34vh',
+        zoom: 4,
+      }}
+    >
+      <h1>{hex}</h1>
+      <button onClick={randomizedHex}>Randomize</button>
+      <button onClick={() => navigator.clipboard.copyText(hex)}>
+        Copy Color
+      </button>
     </div>
   );
 }
